8c01afaf27c01fec2ecca7dceaf3fbe65d310c2c
lochanjangid <lochanjangidcoder@gmail.com>
2026-07-04 17:32:19 +0530

    fix: run_pipeline add parameter target and give it as argument to HandleCategorical.full_hanlder

--- lochan_eda/orchestrator.py
+++ lochan_eda/orchestrator.py
@@ -1,19 +1,14 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 from lochan_eda.numerical import HandleNumerical
 from lochan_eda.categorical import HandleCategorical
 
-class AutomatedEDA(HandleNumerical, HandleCategorical):
+class AutomatedEDA():
     def __init__(self, df):
         self.df = df
 
-    def run_pipeline(self):
+    def run_pipeline(self, target=None):
         num_handler = HandleNumerical(self.df)
         cleaned_num_df = num_handler.full_handler()
         cat_handler = HandleCategorical(self.df)
-        cleaned_cat_df = cat_handler.full_handler()
+        cleaned_cat_df = cat_handler.full_handler(target=target)
 
         return pd.concat([cleaned_num_df, cleaned_cat_df], axis=1, join="inner")
