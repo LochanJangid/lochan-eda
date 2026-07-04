0ae67ca7f8dc4648dfe0f18ec00f9cdef47541d6
lochanjangid <lochanjangidcoder@gmail.com>
2026-07-04 13:17:30 +0530

    feat: outlier handler and scaler they decide technique on specific conditions.

--- lochan_eda/numerical.py
+++ lochan_eda/numerical.py
@@ -1,8 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler, MaxAbsScaler, RobustScaler
+
 class HandleNumerical:
+
     def __init__(self, num_df):
         self.num_df = num_df.copy()
 
-    def imputer(self):
+    def imputer(self, exclude: list):
         """impute missing values based on their data behaviour."""
 
         missing_prcnt = self.num_df.isna().mean() * 100
@@ -26,3 +33,74 @@ class HandleNumerical:
           self.num_df[simple_cols] = self.num_df[simple_cols].fillna(fill_vals)
         
         return self.num_df
+    
+
+    def outlier_manager(self, exclude=None):
+      for col in self.num_df.columns:
+        # finding outlier percentage by iqr method
+        Q1 = self.num_df[col].quantile(0.25)
+        Q3 = self.num_df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - IQR*1.5
+        upper_bound = Q3 + IQR*1.5
+        outliers_count = self.num_df[col][(self.num_df[col] > upper_bound) | (self.num_df[col] < lower_bound)].count()
+        outliers_prcnt = (outliers_count / self.num_df[col].shape[0]) * 100
+        # print(outliers_prcnt)
+        
+        print(outliers_prcnt, col)
+        # when we have only some outliers they can be error ( < 3%). and trimming them will not damage our dataset
+        if outliers_prcnt <= 3.0:
+          self.num_df[col] = self.num_df[col][(self.num_df[col] <= upper_bound) & (self.num_df[col] >= lower_bound)]
+          continue
+
+        # now outliers are so much so we can't just trim them we need to handle them softly :)
+        p90 = self.num_df[col].quantile(0.90)
+        p99 = self.num_df[col].quantile(0.99)
+        max_val = self.num_df[col].max()
+        gap = (p99 - p90) / (max_val - p99)
+
+        if gap <= 1.0:
+          # spikes -> heavy tail. method -> Winsorization
+          p5 = self.num_df[col].quantile(0.05)
+          p95 = self.num_df[col].quantile(0.95)
+          self.num_df[col] = self.num_df[col].clip(lower=p5, upper=p95)
+        else:
+          # Softly -> Long tail. method -> Transformation
+          is_negative_exist = self.num_df[col][self.num_df[col] < 0].count()
+          if not is_negative_exist:
+            is_zero_exist = self.num_df[col][self.num_df[col] == 0].count()
+            if is_zero_exist:
+              # Square root transformation
+              self.num_df[col] = np.sqrt(self.num_df[col])
+            else:
+              # Log transformation
+              self.num_df[col] = np.log1p(self.num_df[col])
+      return self.num_df
+    
+    def scaler(self):
+      for col in self.num_df.columns:
+        # percentage of zero values
+        sparsity = (self.num_df[col] == 0).mean()
+        # measure the skewness
+        skewness = self.num_df[col].skew()
+         # finding outlier percentage by iqr method
+        Q1 = self.num_df[col].quantile(0.25)
+        Q3 = self.num_df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - IQR*1.5
+        upper_bound = Q3 + IQR*1.5
+        outliers_ratio = ((self.num_df[col] > upper_bound) | (self.num_df[col] < lower_bound)).mean()
+
+        if sparsity >= 0.5:
+          scaler_obj = MaxAbsScaler()
+        elif skewness > 1.0 and self.num_df[col].min() >= 0:
+          self.num_df[col] = np.log1p(self.num_df[col])
+          scaler_obj = StandardScaler()
+        elif outliers_ratio >= 0.05:
+          scaler_obj = RobustScaler()
+        else:
+          scaler_obj = StandardScaler()
+        
+        self.num_df[col] = scaler_obj.fit_transform(self.num_df[[col]]).flatten()
+        
+      return self.num_df
