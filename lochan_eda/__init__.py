1586e23871e04c115db5a6a02d707584006a4fd2
lochanjangid <lochanjangidcoder@gmail.com>
2026-07-04 17:09:44 +0530

    importing all module and update version

--- lochan_eda/__init__.py
+++ lochan_eda/__init__.py
@@ -1,2 +1,7 @@
-__version__ = "0.0.1"
+from .orchestrator import AutomatedEDA
+from .numerical import HandleNumerical
+from .categorical import HandleCategorical
+
+__all__ = ["AutomatedEDA", "HandleNumerical", "HandleCategorical"]
+__version__ = "0.0.2"
 __author__ = "Lochan Jangid"
