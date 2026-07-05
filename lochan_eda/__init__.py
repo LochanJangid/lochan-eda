e61c9a47b6f96dba3a6cc7ba8397458a835c442a
lochanjangid <lochanjangidcoder@gmail.com>
2026-07-05 16:27:17 +0530

    model update v0.1.0 to v0.1.1

--- lochan_eda/__init__.py
+++ lochan_eda/__init__.py
@@ -3,5 +3,5 @@ from .numerical import HandleNumerical
 from .categorical import HandleCategorical
 
 __all__ = ["AutomatedEDA", "HandleNumerical", "HandleCategorical"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Lochan Jangid"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lochan-eda",
-    version="0.1.0",
+    version="0.1.1",
     author="Lochan Jangid",
     description="An automated, leakage-free data preprocessing pipeline for machine learning.",
     long_description=open("README.md").read(),
