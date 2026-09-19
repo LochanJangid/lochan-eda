184dc41a4abbbe43f2fb16ff5a110873464886a2
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-19 14:28:54 +0000

    feat: Add Profiler api

--- lochan_eda/__init__.py
+++ lochan_eda/__init__.py
@@ -1,7 +1,8 @@
 from .orchestrator import AutomatedEDA
+from .profiler import  Profiler
 from .numerical import Numerical
 from .categorical import Categorical
 
-__all__ = ["AutomatedEDA", "Numerical", "Categorical"]
+__all__ = ["AutomatedEDA", "Numerical", "Categorical", "Profiler"]
 __version__ = "0.2.0"
 __author__ = "Lochan Jangid"
