236140cd58c656f0ad6aa631b9d3176ef787e3f1
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-19 08:56:57 +0000

    feat: packages info auto

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
+from typing import List
+from pathlib import Path
 from setuptools import setup, find_packages
 
+def require_packages(filepath: str) -> List[str]:
+    return [pckg.replace("\n", "").strip() for pckg in open(filepath).readlines() if "-e ." not in pckg]
 
 setup(
     name="lochan-eda",
     version="0.1.3",
     author="Lochan Jangid",
     description="An automated, leakage-free data preprocessing pipeline for machine learning.",
-    long_description=open("README.md").read(),
+    long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    python_requires=">=3.8",
-    install_requires=[
-        "pandas==3.0.6",
-        "numpy==2.5.3",
-        "matplotlib==3.11.2",
-        "seaborn==0.13.2",
-        "scikit-learn==1.9.1"
-    ],
+    python_requires=">=3.9",
+    install_requires=require_packages("requirements.txt"),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
