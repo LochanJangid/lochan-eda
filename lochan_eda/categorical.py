f5310657e7f3c19280cc1b317eed4bc94edd2370
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-19 12:23:49 +0000

    feat: better exclude in fit and transform only, is_matching function use

--- lochan_eda/categorical.py
+++ lochan_eda/categorical.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import TargetEncoder
-
-from lochan_eda.utils import get_active_cols
+from lochan_eda.utils import get_active_cols, is_matching
 
 
 class Categorical:
     def __init__(self):
-        self.data = None
         self.is_fitted_ = False
         self.missing_drop_threshold = 40
         self.freq_threshold = 0.05
@@ -21,18 +18,18 @@ class Categorical:
         self.target_encoders_ = {}
         self.te_train_encoded_ = {}
 
-    def imputer(self, exclude=None, learn=False):
+    def imputer(self, learn=False):
         """Impute missing values based on missingness percentage."""
-        active_cols = get_active_cols(self.data.columns, exclude)
+        active_cols = get_active_cols(self.fit_data.columns, exclude=None)
 
         if learn:
-            missing_prcnt = self.data[active_cols].isna().mean() * 100
+            missing_prcnt = self.fit_data[active_cols].isna().mean() * 100
             self.drop_cols_ = missing_prcnt[missing_prcnt > self.missing_drop_threshold].index.tolist()
             remaining_cols = [c for c in active_cols if c not in self.drop_cols_]
 
             for col in remaining_cols:
-                if missing_prcnt[col] <= 10 and self.data[col].dropna().shape[0] > 0:
-                    self.impute_modes_[col] = self.data[col].mode()[0]
+                if missing_prcnt[col] <= 10 and self.fit_data[col].dropna().shape[0] > 0:
+                    self.impute_modes_[col] = self.fit_data[col].mode()[0]
         else:
             self.data = self.data.drop(columns=[c for c in self.drop_cols_ if c in self.data.columns], errors='ignore')
             active_cols = [col for col in active_cols if col not in self.drop_cols_]
@@ -41,37 +38,37 @@ class Categorical:
                 self.data[col] = self.data[col].fillna(fill_val)
 
 
-    def rare_manager(self, exclude=None, learn=False):
+    def rare_manager(self, learn=False):
         """Group low-frequency categories into an 'Other' bin."""
-        active_cols = get_active_cols(self.data.columns, exclude)
+        active_cols = get_active_cols(self.fit_data.columns, exclude=None)
 
         if learn:
             for col in active_cols:
-                freqs = self.data[col].value_counts(normalize=True)
+                freqs = self.fit_data[col].value_counts(normalize=True)
                 self.rare_cats_[col] = freqs[freqs < self.freq_threshold].index.tolist()
         else:
             for col, rare_list in self.rare_cats_.items():
                 if col in active_cols and rare_list:
                     self.data[col] = self.data[col].apply(lambda x: 'Other' if x in rare_list else x)
 
-    def encoder(self, exclude=None, learn=False, target=None):
+    def encoder(self, learn=False):
         """Encode categories to numbers based on cardinality (number of unique values)."""
-        active_cols = get_active_cols(self.data.columns, exclude)
+        active_cols = get_active_cols(self.fit_data.columns, exclude=None)
         encoded_dfs = []
         if learn:
             for col in active_cols:
-                unique_cnt = self.data[col].nunique()
+                unique_cnt = self.fit_data[col].nunique()
                 if unique_cnt <= 2:
                     self.encode_types_[col] = 'binary'
-                    unique_vals = sorted(self.data[col].dropna().unique())
+                    unique_vals = sorted(self.fit_data[col].dropna().unique())
                     self.binary_maps_[col] = {val: i for i, val in enumerate(unique_vals)}
                 elif unique_cnt <= 10:
                     self.encode_types_[col] = 'ohe'
-                    temp_ohe = pd.get_dummies(self.data[col], prefix=col, drop_first=True)
+                    temp_ohe = pd.get_dummies(self.fit_data[col], prefix=col, drop_first=True)
                     self.ohe_columns_[col] = temp_ohe.columns.tolist()
                 else:
                     self.encode_types_[col] = 'freq'
-                    self.freq_maps_[col] = self.data[col].value_counts(normalize=True).to_dict()
+                    self.freq_maps_[col] = self.fit_data[col].value_counts(normalize=True).to_dict()
         else:
             for col in active_cols:
                 etype = self.encode_types_.get(col)
@@ -89,19 +86,28 @@ class Categorical:
                 self.data = pd.concat(encoded_dfs, axis=1)
 
     def fit(self, data, exclude=None, target=None):
-        self.data = data
-        self.imputer(exclude=exclude, learn=True)
-        self.rare_manager(exclude=exclude, learn=True)
-        self.encoder(exclude=exclude, learn=True)
+        self.fit_data = data
+        if exclude is not None and isinstance(exclude, list[str]):
+            self.fit_data = self.fit_data.drop(columns=exclude)
+        self.imputer(learn=True)
+        self.rare_manager(learn=True)
+        self.encoder(learn=True)
         self.is_fitted_ = True
         return self.data
 
     def transform(self, data, exclude=None):
         if not self.is_fitted_:
-            raise ValueError("How can you transform self.data before fit.")
+            raise Exception("How can you transform data before fit.")
+        if exclude is not None and isinstance(exclude, list[str]):
+            data = data.drop(columns=exclude)
+        if not is_matching(self.fit_data, data):
+            raise Exception("Unmatched columns.")
 
         self.data = data
+
         self.imputer(exclude=exclude, learn=False)
         self.rare_manager(exclude=exclude, learn=False)
         self.encoder(exclude=exclude, learn=False)
+        if exclude is not None:
+            self.data = pd.concat([self.data, self.excluded_df], axis=1)
         return self.data
--- lochan_eda/numerical.py
+++ lochan_eda/numerical.py
@@ -2,12 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler, RobustScaler
 
-from lochan_eda.utils import get_iqr_bounds, get_active_cols
+from lochan_eda.utils import get_iqr_bounds, get_active_cols, is_matching
 
 
 class Numerical:
     def __init__(self):
-        self.data = None
         self.is_fitted_ = False
         self.missing_drop_threshold = 40
         self.outlier_threshold = 3.0
@@ -17,33 +16,33 @@ class Numerical:
         self.scalers_ = {}
         self.transforms_ = {}
 
-    def imputer(self, exclude=None, learn=False):
+    def imputer(self, learn=False):
         """impute missing values based on their data behaviour."""
-        active_cols = get_active_cols(self.data.columns, exclude)
+        active_cols = get_active_cols(self.fit_data.columns, exclude=None)
 
         # Runs on training and memorize drop_cols & impute values
         if learn: 
-            missing_prcnt = self.data[active_cols].isna().mean() * 100
+            missing_prcnt = self.fit_data[active_cols].isna().mean() * 100
             # drop columns with > threshold missing data
             self.drop_cols_ = missing_prcnt[missing_prcnt > self.missing_drop_threshold].index.tolist()
 
             active_cols = [col for col in active_cols if col not in self.drop_cols_]
-            self.data.drop(columns=self.drop_cols_, inplace=True, errors="ignore")
+            self.fit_data.drop(columns=self.drop_cols_, inplace=True, errors="ignore")
 
             if active_cols:
-                uniquness = (self.data[active_cols].nunique() / self.data[active_cols].shape[0]) * 100
+                uniquness = (self.fit_data[active_cols].nunique() / self.fit_data[active_cols].shape[0]) * 100
                 like_cat = uniquness[uniquness < 1].index
 
                 for col in like_cat:
-                    if self.data[col].isna().sum() > 0:
-                        self.impute_values_[col] = self.data[col].mode().iloc[0]
+                    if self.fit_data[col].isna().sum() > 0:
+                        self.impute_values_[col] = self.fit_data[col].mode().iloc[0]
 
                 simple_cols = [col for col in active_cols if col not in like_cat]
 
                 for col in simple_cols:
                     if missing_prcnt[col] > 0:
-                        skewness = self.data[col].skew()
-                        fill_val = self.data[col].mean() if abs(skewness) < 0.5 else self.data[col].median()
+                        skewness = self.fit_data[col].skew()
+                        fill_val = self.fit_data[col].mean() if abs(skewness) < 0.5 else self.fit_data[col].median()
                         self.impute_values_[col] = fill_val
         
         else:
@@ -56,31 +55,31 @@ class Numerical:
                 if col in self.data.columns:
                     self.data[col] = self.data[col].fillna(fill_val)
 
-    def outlier_manager(self, exclude=None, learn=False):
-        active_cols = get_active_cols(self.data.columns, exclude)
+    def outlier_manager(self, learn=False):
+        active_cols = get_active_cols(self.fit_data.columns, exclude=None)
 
         if learn:
             for col in active_cols:
-                lower_bound, upper_bound = get_iqr_bounds(self.data[col])
-                outliers_prcnt = ((self.data[col] > upper_bound) | (self.data[col] < lower_bound)).mean() * 100
+                lower_bound, upper_bound = get_iqr_bounds(self.fit_data[col])
+                outliers_prcnt = ((self.fit_data[col] > upper_bound) | (self.fit_data[col] < lower_bound)).mean() * 100
 
                 if 0 < outliers_prcnt <= self.outlier_threshold:
                     self.outlier_rules_[col] = {'type': 'clip', 'lower': lower_bound, 'upper': upper_bound}
                 elif outliers_prcnt > self.outlier_threshold:
-                    p90 = self.data[col].quantile(0.90)
-                    p99 = self.data[col].quantile(0.99)
-                    max_val = self.data[col].max()
+                    p90 = self.fit_data[col].quantile(0.90)
+                    p99 = self.fit_data[col].quantile(0.99)
+                    max_val = self.fit_data[col].max()
                     gap = (p99 - p90) / (max_val - p99 + 1e-9)
 
-                    is_negative = (self.data[col] < 0).any()
-                    is_non_negative = (self.data[col] >= 0).all()
+                    is_negative = (self.fit_data[col] < 0).any()
+                    is_non_negative = (self.fit_data[col] >= 0).all()
 
                     if gap <= 1.0 or is_negative:
-                        p5 = self.data[col].quantile(0.05)
-                        p95 = self.data[col].quantile(0.95)
+                        p5 = self.fit_data[col].quantile(0.05)
+                        p95 = self.fit_data[col].quantile(0.95)
                         self.outlier_rules_[col] = {'type': 'clip', 'lower': p5, 'upper': p95}
                     else:
-                        is_zero = (self.data[col] == 0).any()
+                        is_zero = (self.fit_data[col] == 0).any()
                         self.outlier_rules_[col] = {'type': 'sqrt' if is_zero and is_non_negative else 'log1p'}
         else:
             # now Transform data based on learnt things
@@ -93,25 +92,25 @@ class Numerical:
                     elif rule['type'] == 'log1p':
                         self.data[col] = np.log1p(self.data[col])
 
-    def scaler(self, exclude=None, learn=False):
-        active_cols = get_active_cols(self.data.columns, exclude)
+    def scaler(self, learn=False):
+        active_cols = get_active_cols(self.fit_data.columns, exclude=None)
 
         if learn:
             for col in active_cols:
-                sparsity = (self.data[col] == 0).mean()
-                skewness = self.data[col].skew()
-                lower_bound, upper_bound = get_iqr_bounds(self.data[col])
-                outliers_ratio = ((self.data[col] > upper_bound) | (self.data[col] < lower_bound)).mean()
+                sparsity = (self.fit_data[col] == 0).mean()
+                skewness = self.fit_data[col].skew()
+                lower_bound, upper_bound = get_iqr_bounds(self.fit_data[col])
+                outliers_ratio = ((self.fit_data[col] > upper_bound) | (self.fit_data[col] < lower_bound)).mean()
 
                 if sparsity >= 0.5:
-                    scaler_obj, fit_data = MaxAbsScaler(), self.data[[col]]
-                elif skewness > 1.0 and self.data[col].min() >= 0:
+                    scaler_obj, fit_data = MaxAbsScaler(), self.fit_data[[col]]
+                elif skewness > 1.0 and self.fit_data[col].min() >= 0:
                     self.transforms_[col] = "log1p"
-                    scaler_obj, fit_data = StandardScaler(), np.log1p(self.data[[col]])
+                    scaler_obj, fit_data = StandardScaler(), np.log1p(self.fit_data[[col]])
                 elif outliers_ratio >= 0.05:
-                    scaler_obj, fit_data = RobustScaler(), self.data[[col]]
+                    scaler_obj, fit_data = RobustScaler(), self.fit_data[[col]]
                 else:
-                    scaler_obj, fit_data = StandardScaler(), self.data[[col]]
+                    scaler_obj, fit_data = StandardScaler(), self.fit_data[[col]]
 
                 scaler_obj.fit(fit_data)
                 self.scalers_[col] = scaler_obj
@@ -124,21 +123,27 @@ class Numerical:
                         data = np.log1p(data)
                     self.data[col] = scaler_obj.transform(data).flatten()
 
-    def fit(self, data, exclude):
-        self.data = data
-        self.imputer(exclude=exclude, learn=True)
-        self.outlier_manager(exclude=exclude, learn=True)
-        self.scaler(exclude=exclude, learn=True)
+    def fit(self, data, exclude=None):
+        self.fit_data = data
+        if exclude is not None and isinstance(exclude, list[str]):
+            self.fit_data = self.fit_data.drop(columns=exclude)
+        self.imputer(learn=True)
+        self.outlier_manager(learn=True)
+        self.scaler(learn=True)
         self.is_fitted_ = True
         return None
 
-    def transform(self, data, exclude):
+    def transform(self, data, exclude=None):
         ## RAISE ERROR if transform run before fit
         if not self.is_fitted_:
             raise Exception("How can you transform data before fit.")
-
+        if exclude is not None and isinstance(exclude, list[str]):
+            data = data.drop(columns=exclude)
+        if not is_matching(self.fit_data, data):
+            raise Exception("Unmatched columns.")
+        
         self.data = data
-        self.imputer(exclude=exclude, learn=False)
-        self.outlier_manager(exclude=exclude, learn=False)
-        self.scaler(exclude=exclude, learn=False)
+        self.imputer(learn=False)
+        self.outlier_manager(learn=False)
+        self.scaler(learn=False)
         return self.data
