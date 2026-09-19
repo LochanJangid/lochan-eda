e7d05dcea8a4cf06c64705d72b187dc535d794e7
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-19 22:37:15 +0000

    feat: requires depedency

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "seaborn",
         "pytest",
         "reportlab"
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
