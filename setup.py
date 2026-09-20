10d1efdde3740f56020f812975e532ac24832a30
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-20 08:52:03 +0000

    v0.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lochan-eda",
-    version="0.2.2",
+    version="0.2.3",
     author="Lochan Jangid",
     description="A reusable Python package that automates exploratory data analysis and preprocessing workflows.",
     long_description=Path("README.md").read_text(encoding="utf-8"),
